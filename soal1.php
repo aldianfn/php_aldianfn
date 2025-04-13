56fbae202f9446ac3f9e57d3da8827687160c759
aldianfn <aldian.1998@gmail.com>
2025-04-13 13:32:08 +0700

    Soal 1 PHP

---
+++ soal1.php
@@ -0,0 +1,26 @@
+<?php
+
+$jml = $_GET['jml'];
+echo "<table border=1>\n";
+for ($a = $jml; $a > 0; $a--) {
+    $total = 0;     // Deklarasi variabel total untuk menyimpan total angka
+    $value = [];    // Deklarasi variabel value untuk menyimpan setiap pengurangan angka dari $jml
+
+    for ($b = $a; $b > 0; $b--) {
+        // Hitung total setiap baris
+        $total += $b;
+        // Simpan nilai pengurangan angka pada array
+        $value[] = $b;
+    }
+
+    // Tampil hasil total
+    echo "<tr><td colspan='$jml'>Total: $total</td></tr>";
+
+    // Tampil hasil pengurangan angka dari $jml
+    echo "<tr>";
+    foreach ($value as $number) {
+        echo "<td>$number</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
